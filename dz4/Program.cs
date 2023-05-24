e7ceee4c8c412d91f0458878e1f22ab83ac53335
Vadim Sobolev <vadsobolev@yandex.ru>
2023-05-25 01:50:51 +0300

    1_N

---
+++ dz4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int new_number = 2;
+
+if (number > 1)
+{
+    while (new_number <= number)
+    {
+        Console.Write(new_number + " ");
+        new_number = new_number + 2;
+    }
+}
